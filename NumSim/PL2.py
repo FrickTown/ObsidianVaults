68f17aba357a1f92667ef7d8d840edd1f69551d4
FrickTown <jakobaefrick@gmail.com>
2026-09-10 15:06:14 +0200

    vault backup: 2026-09-10 15:06:14

--- NumSim/PL2.py
+++ NumSim/PL2.py
@@ -6,11 +6,12 @@ from scipy.integrate import solve_ivp
 
 def solve_ode(t, y):
     yder = np.zeros(2)
-    yder[0] = math.e ** t*math.sin(y[t])
+    yder[0] = y[1]
+    yder[1] = math.e ** t*math.sin(t)
     return yder
 
 t_span = [0, 3]
-y0 = [0, 0]
+y0 = [0, 1]
 sol = solve_ivp(solve_ode, t_span, y0)
 plt.plot(sol.t, sol.y, "o-r")
 plt.show()
