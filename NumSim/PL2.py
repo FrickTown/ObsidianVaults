855eb3fa4bee30a0aaa28aef1ad5947b4115b58d
FrickTown <jakobaefrick@gmail.com>
2026-09-10 20:24:34 +0200

    vault backup: 2026-09-10 20:24:34

--- NumSim/PL2.py
+++ NumSim/PL2.py
@@ -4,9 +4,6 @@ import numpy as np
 from scipy.integrate import solve_ivp
 from collections.abc import Callable
 
-
-
-
 def question_1():
     def solve_ode(t, y):
         yder = np.zeros(1)
@@ -57,9 +54,31 @@ def question_3():
         yder[0] = y[1]
         yder[1] = y[2]
         yder[2] = (-math.sin(t) * y[0] * y[2]) - (y[0] + t)
+        return yder
+
+    y0 = [0, 0, 5]
+    t_span = [0, 3]
+    sol = solve_ivp(solve_ode, t_span, y0)
+    plt.plot(sol.t, sol.y[0], "o-r", color="red", label="y(t)")
+    plt.plot(sol.t, sol.y[1], "*-r", color="purple", label="y'(t)")
+    plt.plot(sol.t, sol.y[2], "X-r", color="blue", label="y''(t)")
+    plt.legend()
+    plt.show()
+
+def question_4():
+    def custom_ode_solver(fun: Callable[float, float], h: float, t_end: float, y0: float):
+        yi = y0
+        ti = 0
+        k1 = fun(ti, yi)
+        k2 = fun(ti + (h / 2), yi + (h / 2) * k1)
+        k3 = fun(ti + h, yi - (h * k1) + (2 * h * k2))
+    k1: float =
+
 def main():
-    question_1()
-    question_2()
+    #question_1()
+    #question_2()
+    #question_3()
+    question_4()
 
 if __name__ == "__main__":
     main()
