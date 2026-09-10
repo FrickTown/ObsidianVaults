221d895f4c559236480b78e3de3b95771b1284ca
FrickTown <jakobaefrick@gmail.com>
2026-09-10 17:34:18 +0200

    vault backup: 2026-09-10 17:34:18

--- NumSim/PL2.py
+++ NumSim/PL2.py
@@ -2,6 +2,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
+from collections.abc import Callable
 
 
 def solve_ode(t, y):
@@ -14,3 +15,21 @@ y0 = [0]
 sol = solve_ivp(solve_ode, t_span, y0)
 plt.plot(sol.t, sol.y[0], "o-r")
 plt.show()
+
+"""
+Calculates y(t) given y(0), step-size, terminating t-value, and y'
+fun = y'(t)
+"""
+def heuns(fun: Callable[float, float], h: float, t_end: float, y0: float):
+    # Init conditions
+    t = 0.0
+    y_prev = y0
+    solution = [(t, y_prev)]
+    while(t <= t_end):
+        k1: float = fun(t, y_prev)
+        k2: float = fun(t + h, y_prev + h * k1)
+        t = t + h
+        y_next: float = y_prev + (h / 2) * (k1 + k2)
+        solution.append((t, y_next))
+        y_prev = y_next
+    return solution
