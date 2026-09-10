bf843eaafe917e2c839c9c24581dbc1af36726c5
FrickTown <jakobaefrick@gmail.com>
2026-09-10 18:15:17 +0200

    vault backup: 2026-09-10 18:15:17

--- NumSim/PL2.py
+++ NumSim/PL2.py
@@ -5,36 +5,50 @@ from scipy.integrate import solve_ivp
 from collections.abc import Callable
 
 
-def solve_ode(t, y):
-    yder = np.zeros(1)
-    yder[0] = math.pow(math.e, t*math.sin(y[0]) )
-    return yder
 
-t_span = [0, 3]
-y0 = [0]
-sol = solve_ivp(solve_ode, t_span, y0)
-plt.plot(sol.t, sol.y[0], "o-r")
-plt.show()
+
+def question_1():
+    def solve_ode(t, y):
+        yder = np.zeros(1)
+        yder[0] = math.pow(math.e, t * math.sin(y[0]) )
+        return yder
+    t_span = [0, 3]
+    y0 = [0]
+    sol = solve_ivp(solve_ode, t_span, y0)
+    plt.plot(sol.t, sol.y[0], "o-r")
+    plt.show()
 
 """
 Calculates y(t) given y(0), step-size, terminating t-value, and y'
-fun = y'(t)
+fun = y'(t) = f(t, y)
 """
 def heuns(fun: Callable[float, float], h: float, t_end: float, y0: float):
     # Init conditions
     t = 0.0
     y_prev = y0
-    solution = [(t, y_prev)]
+    solution = [(t, y_prev)] # Include t0 position
     while(t <= t_end):
         k1: float = fun(t, y_prev)
         k2: float = fun(t + h, y_prev + h * k1)
         t = t + h
+        # Calculate and store t[i+h]
         y_next: float = y_prev + (h / 2) * (k1 + k2)
         solution.append((t, y_next))
         y_prev = y_next
     return solution
 
-def funny(t: float, y: float):
-    return t - y
+def question_2():
+    def given_func(t, y):
+        return math.pow(math.e, t * math.sin(y))
 
-print(heuns(funny, 0.1, 0.2, 1))
+    h = 0.5
+    end = 3
+    y0 = 0
+    print(heuns(given_func, h, end, y0))
+
+def main():
+    question_1()
+    question_2()
+
+if __name__ == "__main__":
+    main()
